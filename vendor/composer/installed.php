012720c429947a71e5d61c02b53f37b1234159e5
Tamim Amiri <tamimamiriku@gmail.com>
2026-03-28 13:33:21 +0430

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'froala/wysiwyg-editor-php-sdk' => array(
+            'pretty_version' => '4.0.19',
+            'version' => '4.0.19.0',
+            'reference' => 'ed14cb2c7aabce1895bf1e98f981cae121c0d22d',
+            'type' => 'php-plugin',
+            'install_path' => __DIR__ . '/../froala/wysiwyg-editor-php-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
